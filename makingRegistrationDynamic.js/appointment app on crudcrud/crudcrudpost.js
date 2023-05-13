2bd776b910a605ed9f24c538813299ce74ab6ec4
vishnuu96 <riyachowdary37@gmail.com>
2023-05-13 16:35:29 +0530

    crudcrud edit user

--- makingRegistrationDynamic.js/appointment app on crudcrud/crudcrudpost.js
+++ makingRegistrationDynamic.js/appointment app on crudcrud/crudcrudpost.js
@@ -1,35 +1,58 @@
 const form = document.querySelector("#my-form")
+let editInd = document.querySelector("#editInd").value
+const parentElem = document.querySelector("#listOfitems")
+const childElem = document.createElement("li")
 form.addEventListener("submit", (e) => {
     e.preventDefault()
     const name = e.target.name.value
     const email = e.target.email.value
     const number = e.target.phone.value
-    const person = {
-        name: name,
-        email: email,
-        number: number,
-        
+
+    if (editInd === '') {
+        const person = {
+            name: name,
+            email: email,
+            number: number,
+
+        }
+
+        axios.post("https://crudcrud.com/api/7bb8b55dd1aa45d0a27a8091aaa1c4e0/appointmentData", person)
+            .then((response) => {
+                showOnscreen(response.data)
+                console.log(response)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
     }
-    axios.post("https://crudcrud.com/api/8a8f567954894612add8108fe1ac785a/appointmentData", person)
-        .then((response) => {
-            showOnscreen(response.data)
-            console.log(response)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
+    else {
+        const person1 = {
+            name: name,
+            email: email,
+            number: number,
+
+        }
+
+        axios.put(`https://crudcrud.com/api/7bb8b55dd1aa45d0a27a8091aaa1c4e0/appointmentData/${editInd}`, person1)
+            .then(() => {
+                axios.get(`https://crudcrud.com/api/7bb8b55dd1aa45d0a27a8091aaa1c4e0/appointmentData/${editInd}`, person1)
+                    .then((response) => {
+                     showOnscreen(response.data)
+                    })
+            })
+    }
 
 })
-async function showOnscreen(person) {
+function showOnscreen(person) {
     const parentElem = document.querySelector("#listOfitems")
     const childElem = document.createElement("li")
 
-    childElem.textContent ="id:"+person._id + "name: " + person.name + " email: " + person.email + " number:  " + person.number
+    childElem.textContent = "id:" + person._id + "name: " + person.name + " email: " + person.email + " number:  " + person.number
 
     const deletebtn = document.createElement("input")
     deletebtn.type = "button"
     deletebtn.value = "delete"
-    
+
 
     const editbtn = document.createElement("input")
     editbtn.type = "button"
@@ -39,29 +62,31 @@ async function showOnscreen(person) {
     childElem.appendChild(editbtn)
 
     parentElem.appendChild(childElem)
-  
 
-    deletebtn.onclick =()=>{
-         axios.delete(`https://crudcrud.com/api/8a8f567954894612add8108fe1ac785a/appointmentData/${person._id}`)
-        .then((response)=>{
-        parentElem.removeChild(childElem)
-        
-        })
-        .catch((err)=>{
-            console.log(err)
-        })
-}
 
+    deletebtn.onclick = () => {
+        axios.delete(`https://crudcrud.com/api/7bb8b55dd1aa45d0a27a8091aaa1c4e0/appointmentData/${person._id}`)
+            .then((response) => {
+                parentElem.removeChild(childElem)
+
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
     editbtn.onclick = () => {
-        localStorage.removeItem(person.name)
-        parentElem.removeChild(childElem)
-        document.querySelector('#name').value = person.name;
-        document.querySelector('#email').value = person.email
-        document.querySelector('#phone').value = person.number
+        axios.get(`https://crudcrud.com/api/7bb8b55dd1aa45d0a27a8091aaa1c4e0/appointmentData/${person._id}`)
+            .then((response) => {
+                editInd = response.data._id
+                document.querySelector('#name').value = response.data.name;
+                document.querySelector('#email').value = response.data.email
+                document.querySelector('#phone').value = response.data.number
+                parentElem.removeChild(childElem)
+            })
     }
 }
 window.addEventListener("DOMContentLoaded", () => {
-    axios.get("https://crudcrud.com/api/8a8f567954894612add8108fe1ac785a/appointmentData")
+    axios.get("https://crudcrud.com/api/7bb8b55dd1aa45d0a27a8091aaa1c4e0/appointmentData")
         .then((response) => {
             for (let i = 0; i < response.data.length; i++) {
                 showOnscreen(response.data[i])
@@ -71,5 +96,6 @@ window.addEventListener("DOMContentLoaded", () => {
             console.log(err)
         })
 })
+
 
 
