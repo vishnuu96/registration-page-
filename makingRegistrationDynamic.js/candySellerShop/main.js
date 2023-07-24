8d11f3d36b7ebe2182b029b97b92da10b8b80b42
vishnuu96 <riyachowdary37@gmail.com>
2023-07-24 11:20:35 +0530

    added table and async await

--- makingRegistrationDynamic.js/candySellerShop/main.js
+++ makingRegistrationDynamic.js/candySellerShop/main.js
@@ -1,8 +1,7 @@
 const form = document.querySelector("#my-form")
 const parentElement = document.getElementById('listOfCandies')
-const childElem = document.createElement('li')
 // adding eventlistener on submit
-form.addEventListener("submit", (event) => {
+form.addEventListener("submit", async (event) => {
     event.preventDefault();
     const name = event.target.name.value;
     const description = event.target.description.value;
@@ -16,106 +15,131 @@ form.addEventListener("submit", (event) => {
         qty: qty
     }
 //  to post the data to crudcrud
-    axios.post("https://crudcrud.com/api/3e8e073252b542bdbe1c30d7140760cb/candies", object)
-        .then((response) => {
-            showCandyOnScreen(response.data);
-            console.log(response.data);
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-})
+try {
+    const response = await axios.post("https://crudcrud.com/api/77bca05a84ea4d70ab43ad21c6868ad3/candies", object);
+    showCandyOnScreen(response.data);
+    console.log(response.data);
+} catch (err) {
+    console.log(err);
+}
+});
 
 // showing candies on screen
-function showCandyOnScreen(obj) {
+  function showCandyOnScreen(obj) {
     const parentElement = document.getElementById('listOfCandies')
-    const childElem = document.createElement('li')
-    childElem.textContent = obj.name + '-' + obj.description + '-' + obj.price + '-' + obj.qty
-
+    // creating table row element
+    let row = document.createElement('tr')
+    // creating table cells
+    let nameCell = document.createElement('td')
+    let priceCell = document.createElement('td')
+    let descriptionCell = document.createElement('td')
+    let quantityCell = document.createElement('td')
+    let buyOneCell = document.createElement('td')
+    let buyTwoCell = document.createElement('td')
+    let buyThreeCell = document.createElement('td')
+    
+    nameCell.textContent = obj.name;
+    descriptionCell.textContent = obj.description;
+    priceCell.textContent = obj.price;
+    quantityCell.textContent = obj.qty;
     // creating buy one button one and functionality to update the quantity value in crudcrud
     const buyOne = document.createElement('input')
     buyOne.type = 'button'
-    buyOne.value = 'buy One'
-    buyOne.onclick = () => {
-        obj.qty -= 1;
-        var temp = {
+    buyOne.value = 'Buy One'
+    buyOne.className="btn btn-success"
+    buyOne.onclick =async () => {
+        obj.qty = obj.qty-1;
+        let temp = {
             name: obj.name,
             price: obj.price,
             description: obj.description,
             qty: `${obj.qty}`
         }
-        axios.put(`https://crudcrud.com/api/3e8e073252b542bdbe1c30d7140760cb/candies/${obj._id}`, temp)
-            .then(() => {
-                childElem.textContent = temp.name + '-' + temp.description + '-' + temp.price + '-' + temp.qty
-                childElem.appendChild(buyOne)
-                childElem.appendChild(buyTwo)
-                childElem.appendChild(buyThree)
-                parentElement.appendChild(childElem)
-            })
+
+        try{
+            await axios.put(`https://crudcrud.com/api/77bca05a84ea4d70ab43ad21c6868ad3/candies/${obj._id}`, temp)
+            quantityCell.textContent = temp.qty;
+        }
+        catch (err){
+            console.log(err)
+        }
+        
+            
     }
 
  // creating buy two button  and functionality to update the quantity value in crudcrud
     const buyTwo = document.createElement('input')
     buyTwo.type = 'button'
-    buyTwo.value = 'buy Two'
-    buyTwo.onclick = () => {
-        obj.qty -= 2;
-        var temp = {
+    buyTwo.value = 'Buy Two'
+    buyTwo.className="btn btn-success"
+    buyTwo.onclick = async () => {
+        obj.qty = obj.qty - 2;
+        let temp = {
             name: obj.name,
             price: obj.price,
             description: obj.description,
             qty: `${obj.qty}`
         }
-        axios.put(`https://crudcrud.com/api/3e8e073252b542bdbe1c30d7140760cb/candies/${obj._id}`, temp)
-        .then(()=>{
-        childElem.textContent = temp.name + '-' + temp.description + '-' + temp.price + '-' + temp.qty;
-        childElem.appendChild(buyOne)
-        childElem.appendChild(buyTwo)
-        childElem.appendChild(buyThree)
-        parentElement.appendChild(childElem)
-    })
+
+        try{
+            await  axios.put(`https://crudcrud.com/api/77bca05a84ea4d70ab43ad21c6868ad3/candies/${obj._id}`, temp)
+            quantityCell.textContent = temp.qty;
+        }
+       catch(err){
+        console.log(err)
+       }
     }
 
     // creating buy two button  and functionality to update the quantity value in crudcrud
     const buyThree = document.createElement('input')
     buyThree.type = 'button'
-    buyThree.value = 'buy Three'
-    buyThree.onclick = () => {
-        obj.qty -= 3;
-        var temp = {
+    buyThree.value = 'Buy Three'
+    buyThree.className="btn btn-success"
+    buyThree.onclick = async () => {
+        obj.qty = obj.qty-3;
+        let temp = {
             name: obj.name,
             price: obj.price,
             description: obj.description,
             qty: `${obj.qty}`
         }
-        axios.put(`https://crudcrud.com/api/3e8e073252b542bdbe1c30d7140760cb/candies/${obj._id}`, temp)
-        .then(()=>{
-        childElem.textContent = temp.name + '-' + temp.description + '-' + temp.price + '-' + temp.qty;
-        childElem.appendChild(buyOne)
-        childElem.appendChild(buyTwo)
-        childElem.appendChild(buyThree)
-        parentElement.appendChild(childElem)
-    })
+
+        try{
+            await axios.put(`https://crudcrud.com/api/77bca05a84ea4d70ab43ad21c6868ad3/candies/${obj._id}`, temp)
+            quantityCell.textContent = temp.qty;
+        }
+        catch(err){
+            console.log(err)
+        }
     }
 
-    childElem.appendChild(buyOne)
-    childElem.appendChild(buyTwo)
-    childElem.appendChild(buyThree)
-    parentElement.appendChild(childElem)
+    buyOneCell.appendChild(buyOne)
+    buyTwoCell.appendChild(buyTwo)
+    buyThreeCell.appendChild(buyThree)
+
+    row.appendChild(nameCell);
+    row.appendChild(descriptionCell);
+    row.appendChild(priceCell);
+    row.appendChild(quantityCell);
+    row.appendChild(buyOneCell);
+    row.appendChild(buyTwoCell);
+    row.appendChild(buyThreeCell);
+ 
+    parentElement.appendChild(row);
+    
 }
 // to show the candies on reloading
-window.addEventListener("DOMContentLoaded", () => {
-    axios.get("https://crudcrud.com/api/3e8e073252b542bdbe1c30d7140760cb/candies")
-        .then((response) => {
-            console.log(response.data);
-
-            for (var i = 0; i < response.data.length; i++) {
-                showCandyOnScreen(response.data[i]);
-            }
-        })
-        .catch((err) => {
+window.addEventListener("DOMContentLoaded", async () => {
+    try{
+       const response= await axios.get("https://crudcrud.com/api/77bca05a84ea4d70ab43ad21c6868ad3/candies")
+    
+       for (var i = 0; i < response.data.length; i++) {
+        showCandyOnScreen(response.data[i]);
+       }
+    }
+    catch{(err) => {
             console.log(err);
-        })
+        }}
 
 })
 
